18ec7f02cf16e4ee66a11456940acf3147a346d1
Fernando <cronosmen@gmail.com>
2016-03-13 19:36:59 +0100

    Changelog:
    Documentación en router,
    Ahora se pueden utilizar middlewares

--- app.js
+++ app.js
@@ -6,8 +6,27 @@
 /**
  *
  * @type {*|exports|module.exports}
- * app is singleton
+ * app.getInstance().run will start the app
  */
 let app = require("./lib/App");
+
+app.getInstance().registerMiddlewares("/", function(req,res,next){
+    console.log("just a middleware");
+    next();
+});
+
+
+app.getInstance().registerMiddlewares("/", function(req,res,next){
+    console.log("just the second middleware on /");
+    next();
+});
+
+app.getInstance().registerMiddlewares("/pepe", function(req, res, next){
+
+    res.statusCode = 500;
+    res.write("Who's pepe?");
+    res.end();
+    return;
+});
 
 app.getInstance().run();
